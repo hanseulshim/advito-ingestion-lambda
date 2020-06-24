cc91802a5b77eb9d868e8c96c8165e9d65bf38d7
Hanseul Shim <hanseulshim@gmail.com>
2020-06-24 16:59:27 -0400

    Update backout to new format

--- backout.js
+++ backout.js
@@ -19,150 +19,61 @@ const advito = require('knex')({
 module.exports.backout = async (event) => {
 	try {
 		const { jobIngestionId } = event
-		const statuses = ['processed', 'loaded', 'approved']
 		const startTime = new Date().getTime()
 		console.log('running for job: ', jobIngestionId)
 
-		const jobIngestion = await advito('job_ingestion')
+		const hotelProject = await advito('hotel_project_job_ingestion')
 			.select()
-			.where('id', jobIngestionId)
+			.where('job_ingestion_id', jobIngestionId)
 			.first()
 
-		if (!jobIngestion) {
-			throw new TypeError('Job Ingestion Hotel not found', '500')
-		} else if (
-			!jobIngestion.is_complete ||
-			!statuses.includes(jobIngestion.job_status)
-		) {
-			throw new Error('Job Ingestion does not have status of complete', '500')
-		} else if (!statuses.includes(jobIngestion.job_status)) {
-			throw new Error('Job Ingestion does not have status of complete', '500')
-		} else if (!statuses.includes(jobIngestion.job_status)) {
-			throw new Error(
-				'Job Ingestion does not have one of the following status: processed, loaded, approved.',
-				'500'
-			)
+		if (!hotelProject) {
+			return true
 		}
 
-		const stageActivityHotelList = await advito('stage_activity_hotel')
-			.where('job_ingestion_id', jobIngestion.id)
-			.select('id')
-
-		const hotelProjectPropertyList = await advito('hotel_project_property')
+		const jobIngestionList = await advito('hotel_project_job_ingestion')
 			.select()
-			.where('agency_job_ingestion_id', jobIngestion.id)
-			.orWhere('cc_job_ingestion_id', jobIngestion.id)
-			.orWhere('supplier_job_ingestion_id', jobIngestion.id)
+			.where('hotel_project_id', hotelProject.hotel_project_id)
 
-		const stageActivityHotelIds = stageActivityHotelList.map((v) => v.id)
+		if (jobIngestionList.length) {
+			const jobQueries = jobIngestionList.map((v) =>
+				advito('job_ingestion_hotel')
+					.update({
+						is_dpm: false,
+						status_dpm: null,
+						date_status_dpm: null,
+						is_sourcing: false,
+						status_sourcing: null,
+						date_status_sourcing: null
+					})
+					.where('job_ingestion_id', v.job_ingestion_id)
+			)
+			await Promise.all(jobQueries)
+		}
 
-		const startTime1 = new Date().getTime()
+		await advito('hotel_project_property_day')
+			.delete()
+			.where('hotel_project_id', hotelProject.hotel_project_id)
+		await advito('hotel_project_property')
+			.delete()
+			.where('hotel_project_id', hotelProject.hotel_project_id)
+		await advito('hotel_project_job_ingestion')
+			.delete()
+			.where('hotel_project_id', hotelProject.hotel_project_id)
+		await advito('hotel_project')
+			.delete()
+			.where('id', hotelProject.hotel_project_id)
+
 		console.log(
-			`Grabbed both lists. stageActivityHotelList length: ${
-				stageActivityHotelList.length
-			}, hotelProjectPropertyList length: ${
-				hotelProjectPropertyList.length
-			}. Run Time: ${startTime1 - startTime}ms`
+			`Deleted everything. Run Time: ${new Date().getTime() - startTime}ms`
 		)
 
-		await Promise.all([
-			advito('activity_hotel')
-				.delete()
-				.whereIn('stage_id', stageActivityHotelIds),
-			advito('stage_activity_hotel_candidate')
-				.delete()
-				.whereIn('stage_activity_hotel_id', stageActivityHotelIds)
-		])
-		const startTime2 = new Date().getTime()
-		console.log(
-			`Deleted activity_hotel and stage_activity_hotel_candidate. Run Time: ${
-				startTime2 - startTime
-			}ms`
-		)
-
-		const agencyList = hotelProjectPropertyList.filter(
-			(v) => v.agencyJobIngestionId !== null
-		)
-		const ccList = hotelProjectPropertyList.filter(
-			(v) => v.ccJobIngestionId !== null
-		)
-		const supplierList = hotelProjectPropertyList.filter(
-			(v) => v.supplierJobIngestionId !== null
-		)
-
-		await Promise.all([
-			advito('stage_activity_hotel')
-				.delete()
-				.whereIn('id', stageActivityHotelIds),
-			advito('hotel_project_property_day')
-				.delete()
-				.whereIn('hotel_property_id', [
-					...new Set(agencyList.map((v) => v.hotel_property_id))
-				])
-				.whereIn('hotel_project_id', [
-					...new Set(agencyList.map((v) => v.hotel_project_id))
-				])
-				.whereRaw('LOWER("template_category") = ?', 'agency'),
-			advito('hotel_project_property_day')
-				.delete()
-				.whereIn('hotel_property_id', [
-					...new Set(ccList.map((v) => v.hotel_property_id))
-				])
-				.whereIn('hotel_project_id', [
-					...new Set(ccList.map((v) => v.hotel_project_id))
-				])
-				.whereRaw('LOWER("template_category") = ?', 'cc'),
-			advito('hotel_project_property_day')
-				.delete()
-				.whereIn('hotel_property_id', [
-					...new Set(supplierList.map((v) => v.hotel_property_id))
-				])
-				.whereIn('hotel_project_id', [
-					...new Set(supplierList.map((v) => v.hotel_project_id))
-				])
-				.whereRaw('LOWER("template_category") = ?', 'supplier'),
-			advito('hotel_project_property')
-				.update({
-					agency_date_currency_conversion: null,
-					agency_room_nights: null,
-					agency_spend_usd: null
-				})
-				.where('agency_job_ingestion_id', jobIngestion.id),
-			advito('hotel_project_property')
-				.update({
-					cc_date_currency_conversion: null,
-					cc_room_nights: null,
-					cc_spend_usd: null
-				})
-				.where('cc_job_ingestion_id', jobIngestion.id),
-			advito('hotel_project_property')
-				.update({
-					supplier_date_currency_conversion: null,
-					supplier_room_nights: null,
-					supplier_spend_usd: null
-				})
-				.where('supplier_job_ingestion_id', jobIngestion.id)
-		])
-		console.log(
-			`Deleted everything else. Run Time: ${
-				new Date().getTime() - startTime2
-			}ms`
-		)
 		return true
 	} catch (e) {
-		if (e.name !== 'TypeError') {
-			const { jobIngestionId, jobStatus } = event
-			await advito('job_ingestion')
-				.update({
-					job_status: jobStatus
-				})
-				.where('id', jobIngestionId)
-		}
 		throw e
 	}
 }
 
 // module.exports.backout({
-// 	jobIngestionId: 18654,
-// 	jobStatus: 'test'
+// 	jobIngestionId: 18873
 // })
