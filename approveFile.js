39ba872cff09317e567297ba16e754420eaa5ace
Hanseul Shim <hanseulshim@gmail.com>
2020-07-13 13:25:03 -0400

    Update approveFile

--- approveFile.js
+++ approveFile.js
@@ -2,7 +2,7 @@
 
 const yenv = require('yenv')
 const env = yenv('./env.yml', {
-	env: 'default'
+	env: 'production'
 })
 const advito = require('knex')({
 	client: 'pg',
@@ -16,29 +16,64 @@ const advito = require('knex')({
 	}
 })
 
+const hotel = require('knex')({
+	client: 'pg',
+	connection: {
+		user: process.env.DB_USER ? process.env.DB_USER : env.DB_USER,
+		host: process.env.DB_HOST ? process.env.DB_HOST : env.DB_HOST,
+		database: process.env.DB_HOTEL ? process.env.DB_HOTEL : env.DB_HOTEL,
+		password: process.env.DB_PASSWORD
+			? process.env.DB_PASSWORD
+			: env.DB_PASSWORD
+	}
+})
+
 module.exports.approveFile = async (event, context) => {
 	context.callbackWaitsForEmptyEventLoop = false
 	try {
-		const { jobIngestionId, clientId, type } = event
-		if (!jobIngestionId) {
+		const { jobIngestionIds, clientId, type, userId } = event
+		if (!jobIngestionIds.length) {
 			throw Error('Job ingestion not found')
 		}
-		await advito.raw(
-			`select * from approve_for_sourcing_dpm(${jobIngestionId}, ${clientId}, '${type}')`
+		const { rows } = await advito.raw(
+			`SELECT * FROM approve_for_sourcing_dpm(ARRAY[${jobIngestionIds}], ${clientId}, '${type}')`
 		)
+		console.log(rows)
+		if (rows.length) {
+			const hotelProject = rows[0]
+			if (type === 'dpm') {
+				await hotel.raw(`SELECT process_hpd(${hotelProject.id}, ${userId})`)
+			} else {
+				await hotel.raw(
+					`SELECT process_spend_analysis(${hotelProject.id}, ${userId})`
+				)
+				await hotel.raw(`SELECT approve_hpm(${hotelProject.id})`)
+				await hotel.raw(`SELECT update_bar_rates(${hotelProject.project_year})`)
+				await hotel.raw(`SELECT update_bcd_rates(${hotelProject.project_year})`)
+			}
+		} else {
+			console.log('Hotel project not found... something went wrong!')
+			throw new Error('Hotel project not found... something went wrong!')
+		}
+
+		console.log('done')
 
 		return true
 	} catch (e) {
 		console.log(e.message)
 		await advito('job_ingestion_hotel')
 			.update('ingestion_note', 'error')
-			.where('job_ingestion_id', event.jobIngestionId)
+			.whereIn('job_ingestion_id', event.jobIngestionIds)
 		throw Error('Loading enhanced QC failed')
 	}
 }
 
-// module.exports.approveFile({
-// jobIngestionId: 18862,
-// clientId: 348,
-// type: 'sourcing'
-// })
+// module.exports.approveFile(
+// 	{
+// 		jobIngestionIds: [744, 777],
+// 		clientId: 347,
+// 		type: 'sourcing',
+// 		userId: 882
+// 	},
+// 	{}
+// )
